df2425a93c4c66f3144d7ad89007898671bba867
Ean McDonald Wojciechowski <ean@Eans-MacBook-Pro.local>
2018-11-18 14:17:35 -0500

    Hi Bandrei

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return(
+        <div>
+            <div className="Header">University Comparer</div>
+            <button> Search</button>
+            <button> Profile</button>
+            <button> Settings</button>
+
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Login.css";
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+}
