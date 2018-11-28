563e2387b4dcdca278f58bd6193a8517c99080a2
Ean McDonald Wojciechowski <ean@Eans-MacBook-Pro.local>
2018-11-27 22:31:05 -0500

    dsds

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,6 +4,7 @@ import { FormGroup, Form, Row, Col, FormControl, Button,
   Media, Grid, } from 'react-bootstrap';
 import imggg from './listtt.png';
 import './Profile.css';
+import PropTypes from 'prop-types';
 
 
 class Profile extends Component {
@@ -11,23 +12,25 @@ class Profile extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      firstName: 'Your first name',
-      lastName: 'Your last name',
-      email: 'email',
-      username: 'Username',
-      password: '*******',
-      gpa: '0.0',
-      act: '0',
-      sat: '0',
-      gender: '',
-      age: '',
-      hometown: '',
+      isEditing: false,
+      firstName: 'Your first name' ,
+      lastName: 'Your last name' ,
+      email:   'email',
+      username:   'Username',
+      password:'*******',
+      gpa:  0.0,
+      act:  '0',
+      sat:  '0',
+      gender:   '',
+      age:  0,
+      hometown:   '',
 
 
 
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.toggleEdit = this.toggleEdit.bind(this)
   }
 
   handleChange (event) {
@@ -41,7 +44,147 @@ class Profile extends Component {
     event.preventDefault()
   }
 
+  toggleEdit() {
+    this.setState({isEditing: !this.state.isEditing})
+  }
+
   render() {
+  if (!this.state.isEditing) {
+    return(
+      <div> 
+        <Grid>
+          <Row>
+              <Col xs='3'>
+                <Media>
+                  <img width={120} height={120} src={imggg} />
+                </Media>
+              </Col>
+              <Col xs='9' className="name">
+                NAME
+              </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2' className='fields'>
+              <label htmlFor='firstName'>
+                First Name
+              </label>
+            </Col>
+            <Col xs='8'>
+              <p>{this.state.firstName}</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2'>
+              <label htmlFor='lastName'>
+                Last Name
+              </label>
+            </Col>
+            <Col xs='8'>
+              {this.state.lastName}
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2'>
+              <label htmlFor='email'>
+                Email
+              </label>
+            </Col>
+            <Col xs='8'>  
+              <p>{this.state.email}</p> 
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2'>
+              <label htmlFor='gpa'>
+                GPA
+              </label>
+            </Col>
+            <Col xs='8'>
+              <p>{this.state.gpa}</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2'>
+              <label htmlFor='act'>
+                ACT
+              </label>
+            </Col>
+            <Col xs='8'>          
+              <p>{this.state.act}</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2'>
+              <label htmlFor='sat'>
+                SAT
+              </label>
+            </Col> 
+            <Col xs='8'>  
+              <p>{this.state.sat}</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2'>
+              <label htmlFor='gender'>
+                Gender
+              </label>
+            </Col>
+            <Col xs='8'>
+              <p>{this.state.gender}</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2'>
+              <label htmlFor='age'>
+                Age
+              </label>
+            </Col>
+            <Col xs='8'>  
+              <p>{this.state.age}</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='2'>
+              <label htmlFor='hometown'>
+                Hometown
+              </label>
+            </Col>
+            <Col xs='8'>
+              <p>{this.state.hometown}</p>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs='2' />
+            <Col xs='8'>
+              <button onClick={this.toggleEdit}>edit</button>
+            </Col>
+
+          </Row>
+
+      </Grid>
+      </div>
+    )
+  }
+
     return (
       <Grid>
           <Row>
@@ -163,10 +306,21 @@ class Profile extends Component {
             </Col>
           </Row>
 
+          <Row>
+            <Col xs='2' />
+            <Col xs='8'>
+              <button onClick={this.toggleEdit}>edit</button>
+            </Col>
+          </Row>
+
       </Grid>
     );
-  }
+    }
+
 }
+
+
+
 
 export default Profile;
 
