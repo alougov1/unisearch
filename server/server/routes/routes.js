4b4a83330697f291cd421423f58be0c861feee89
alougov1 <lougovtsov.an@husky.neu.edu>
2018-11-30 13:41:29 -0500

    basic fetch

--- server/server/routes/routes.js
+++ server/server/routes/routes.js
@@ -1,5 +1,22 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host     : 'unisearch.cw7uxohppzrc.us-east-1.rds.amazonaws.com',
+  port      :  '3306',
+  user     : 'alougov1',
+  password : 'iamroot123',
+  database : 'unisearch'
+
+});
+
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... ");
+} else {
+    console.log("Error connecting to database ... ");
+}
+});
+
 var appRouter = function (app) {
   app.get("/", function(req, res) {
     res.status(200).send("Welcome to our restful API");
